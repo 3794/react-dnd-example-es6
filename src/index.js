1ca59576fa7d60d9d200bc54ef269644cf7afeb8
hyo <hyo.won@kakaocorp.com>
2018-04-16 18:55:42 +0900

    init

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './custom-drag-layer';
+
+ReactDOM.render(<App />, document.getElementById('root'));
